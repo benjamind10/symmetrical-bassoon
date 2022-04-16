f24b770276d8cfbbf9e34202f9222e7452b70f70
Ben <benjamind10@pm.me>
2022-04-16 12:05:34 -0400

    added heroku env

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const APP_PREFIX = 'budget_tracker';
-const VERSION = 'version_01';
+const VERSION = '01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = ['./index.html', './css/styles.css', './js/index.js'];
 
